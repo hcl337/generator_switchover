8eae2325a0d28489675075244cb3d8292683d08d
Hans Lee <hansclee@gmail.com>
2020-02-08 20:58:55 -0800

    Updated to work

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import platform
 import threading
 from scripts import generator_loggers
 
-is_simulator = "Darwin" in platform.platform()
+is_simulator = True or "Darwin" in platform.platform()
 params = {}
 
 if not is_simulator:
@@ -54,7 +54,7 @@ def get_battery_voltage( ):
 
     voltage =  load_simulator()["analog.one"] if is_simulator else automationhat.analog.one.read()
 
-    return voltage*params["voltage_divider"]
+    return voltage*params["voltage_multiplier"] + params["voltage_offset"]
 
 
 def get_ac_current_amps( ):
@@ -68,18 +68,18 @@ def get_ac_current_amps( ):
 
     amps = load_simulator()["analog.two"] if is_simulator else automationhat.analog.two.read()
 
-    return amps*params["ac_current_divider"]
+    return  amps*amps*params["ac_current_multiplier_2"] + amps*params["ac_current_multiplier"] + params["ac_current_offset"]
 
 
 def set_generator( state ):
     '''
 
     '''
-
-    if state is "on":
-        automationhat.relay.one.on()
-    elif state is "off":
-        automationhat.relay.one.off()
+    if not is_simulator:
+        if state is "on":
+            automationhat.relay.one.on()
+        elif state is "off":
+            automationhat.relay.one.off()
 
 
 def is_voltage_low( ):
@@ -137,6 +137,7 @@ def threaded_charge_batteries( ):
             # If we have a low voltage, set the start time
             # to now if we haven't started
             if time_of_low_voltage is None:
+                logger.info("Voltage is below minimum. Starting countdown")
                 time_of_low_voltage = time()
 
             elapsed_low_voltage_time = time() - time_of_low_voltage
@@ -144,9 +145,11 @@ def threaded_charge_batteries( ):
             if elapsed_low_voltage_time > params["low_voltage_secs_before_generator"]:
                 time_of_low_voltage = None
                 charge_batteries_with_generator( )
-        else:
+                sleep(3600) # Don't try again for an hour
+        elif time_of_low_voltage is not None:
             time_of_low_voltage = None
-
+            logger.info("Voltage is above minimum again. Canceling countdown")
+ 
 
 def charge_batteries_with_generator( ):
     '''
@@ -167,14 +170,16 @@ def charge_batteries_with_generator( ):
         # STEP 1 - BRING TO 57.6 V. THIS MAY TAKE AN HOUR?
         ######################################################################
 
-        while elapsed < params["maximum_time_charging_to_full_voltage"]:
-            elapsed = time() - start_time()
+        while elapsed < params["maximum_seconds_charging_to_full_voltage"]:
+            sleep(10)
+            elapsed = time() - start_time
             if get_battery_voltage() > params["full_charge_voltage"]:
                 logger.info("Full voltage during charging after " + str(elapsed/60) + " minutes.")
                 break
         else:
             logger.error("Did not get to full voltage in maximum time: " + str(get_battery_voltage()) + " after " + str(elapsed))
-
+            return
+            
         ######################################################################
         # STEP 2 - CHARGE AT THAT VOLTAGE FOR A PERIOD OF TIME
         ######################################################################
@@ -185,6 +190,10 @@ def charge_batteries_with_generator( ):
         while elapsed_trickle_charge < params["trickle_charge_time"]:
             sleep(60)
             logger.debug("    " + str(round(elapsed/60)) + " min. Voltage: " + str(round(get_battery_voltage(), 1)))
+            
+            if get_battery_voltage() > params["over_voltage"]:
+                logger.info("    Battery hit maximum voltage so stopping charge")
+                break
         
         logger.info("CHARGE COMPLETE AFTER " + str(charge_minutes) + " minutes.")
     except Exception as e:
@@ -193,6 +202,7 @@ def charge_batteries_with_generator( ):
         logger.info("Ended charging after " + str(elapsed/60) + " minutes.")
         set_generator("off")
 
+
 def threaded_display( ):
 
     while True:
@@ -200,6 +210,17 @@ def threaded_display( ):
         logger.info("Voltage: " + str(round(get_battery_voltage(),1)) + " volts. Current: " + str(round(get_ac_current_amps(),2)) + " amps.")
 
 
+def self_test( ):
+    logger.debug("Turning on generator")
+    set_generator("on")
+    sleep(2)
+    set_generator("off")
+    logger.debug("Turning off generator")
+    
+    sleep(1)
+    logger.debug("Done with self test")
+    
+    
 if __name__ == "__main__":
     
     logger = logging.getLogger("generator")
@@ -217,6 +238,8 @@ if __name__ == "__main__":
     for p in params:
         logger.info("    " + p + ": " + str(params[p]))
     logger.info("")
+    
+    self_test()
 
     thread_current = threading.Thread(target=threaded_measure_current, name='Measure Current', daemon=True)
     thread_current.start()
